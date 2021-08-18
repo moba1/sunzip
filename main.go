3e7f48790635dd2bce01d217b82796fc9f0b86f7
moba1 <moba2@protonmail.com>
2021-08-18 23:28:09 +0900

    add output directory argument

--- main.go
+++ main.go
@@ -10,14 +10,48 @@ import (
 	"os"
 	"path"
 	"strconv"
+	"flag"
 )
 
-var logger = log.New(os.Stderr, "", 0)
+var (
+	logger = log.New(os.Stderr, "", 0)
+	outDir = "."
+)
 
 func exit(msg interface{}) {
 	logger.Println(msg)
 	os.Exit(1)
 }
+
+func init() {
+	flag.Usage = func() {
+		prog := path.Base(os.Args[0])
+		out := func (format string, a ...interface{}) {
+			fmt.Fprintf(flag.CommandLine.Output(), format+"\n", a...)
+		}
+		out("Usage of %s", prog)
+		out("\t%s [outDir]", prog)
+		out("Args:")
+		out("\toutDir\t output root directory. if this argument is set, all files and directories will be extracted to the specified directory. (default: \".\"")
+		out("Example:")
+		out("\t$ echo foo > bar.txt")
+		out("\t$ zip baz.zip bar.txt")
+		out("\t$ cat baz.zip | %s extracted", prog)
+		out("\t$ ls extracted")
+		out("\tbar.txt")
+		out("\t$ cat extracted/bar.txt")
+		out("\tfoo")
+	}
+	flag.Parse()
+	args := flag.Args()
+	if len(args) >= 1 {
+		outDir = args[0]
+		if err := os.MkdirAll(outDir, 0755); err != nil {
+			exit(fmt.Sprintf("can't create output directory (reason: %s)", strconv.Quote(err.Error())))
+		}
+	}
+}
+
 
 func printWarning(msg interface{}) {
 	logger.Printf("warning: %v\n", msg)
@@ -59,7 +93,7 @@ func main() {
 
 	for _, f := range r.File {
 		if f.FileInfo().IsDir() {
-			if err := os.MkdirAll(path.Join(".", f.Name), f.Mode()); err != nil {
+			if err := os.MkdirAll(path.Join(outDir, f.Name), f.Mode()); err != nil {
 				printWarning(fmt.Sprintf("can't make directory %s (reason: %s)", strconv.Quote(f.Name), strconv.Quote(err.Error())))
 			} else {
 				fmt.Println(f.Name)
@@ -73,10 +107,10 @@ func main() {
 			continue
 		}
 
-		if err := os.MkdirAll(path.Join(".", path.Dir(f.Name)), os.ModeDir); err != nil {
+		if err := os.MkdirAll(path.Join(outDir, path.Dir(f.Name)), 0755); err != nil {
 			printWarning(fmt.Sprintf("can't make directory %s (reason: %s)", strconv.Quote(path.Dir(f.Name)), strconv.Quote(err.Error())))
 		}
-		w, err := os.OpenFile(path.Join(".", f.Name), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		w, err := os.OpenFile(path.Join(outDir, f.Name), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
 		if err != nil {
 			printWarning(fmt.Sprintf("can't open file %s (reason: %s)", strconv.Quote(f.Name), strconv.Quote(err.Error())))
 			continue
